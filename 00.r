e0f8164a01cb6f46decf21fe810c442fd14e70a1
Developer <klevtsovstanislav@gmial.com>
2016-03-05 23:34:17 +0300

    Create 00.r

---
+++ 00.r
@@ -0,0 +1,16 @@
+#addten <- function(x){
+#  return(x+10)
+#}
+#morecats <- addten(cats)
+#morecats
+
+#addten <-function(x){
+#  moredogs <<-x+10
+#}
+
+#addten(dogs)
+#dogs
+#ls()
+#rm(list =ls())
+print(log2(sin (atan(1))^2))
+
