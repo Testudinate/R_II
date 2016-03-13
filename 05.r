292805d02a00c0de242a7cee816ea9f7f13e0ee7
Developer <klevtsovstanislav@gmial.com>
2016-03-13 18:27:23 +0300

    Create 05.r

---
+++ 05.r
@@ -0,0 +1,18 @@
+make.names("next")
+
+#> make.names("xXx_TerM1Nat0R_xXx")
+#[1] "xXx_TerM1Nat0R_xXx"
+#> make.names("function")
+#[1] "function."
+#> make.names(":smiley:")
+#[1] "X.smiley."
+#> make.names("super_long_name_in_fact_so_long_i_cannot_stop_typing_please_help")
+#[1] "super_long_name_in_fact_so_long_i_cannot_stop_typing_please_help"
+#> make.names("2b.or.not.2b")
+#[1] "X2b.or.not.2b"
+#> make.names(".hidden")
+#[1] ".hidden"
+#> make.names("next")
+#[1] "next."
+
+
